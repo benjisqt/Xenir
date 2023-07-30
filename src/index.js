dd9f35c0027aa32f5785b70d93e228540970196e
benjis <128649752+benjisqt@users.noreply.github.com>
2023-07-30 23:50:05 +0100

    ✅ new: added mongodb connection :)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
+const mongoose = require('mongoose');
 const log = require('./Functions/logger');
 const startcmdhandler = require('./Functions/cmdhandler');
 const starteventhandler = require('./Functions/eventhandler');
+
+// CONNECTION METHODS: MongoDB (more coming soon!)
+const connectionmode = 'mongo';
 
 const client = new Discord.Client({
     intents: [
@@ -21,4 +25,18 @@ client.commands = new Discord.Collection();
 client.login(config.token).then(async () => {
     startcmdhandler(client);
     starteventhandler(client);
+    if(connectionmode === 'mongo') {
+        try {
+            const connection = await mongoose.connect(config.database);
+            if(connection) {
+                log(`XenirDB connected successfully.`);
+            } else {
+                return log(`Mongoose wasn't able to connect for an unknown reason.`, true);
+            }
+        } catch (err) {
+            return log(`Mongoose wasn't able to connect: ${err}`, true);
+        }
+    } else {
+        return log(`That is not a valid database connection method. Try: 'mongo'`, true);
+    }
 });
